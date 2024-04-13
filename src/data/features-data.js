a8a34ade6d06e6132ec8c2d7e8504ac02b09a63e
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-04-12 22:25:30 -0500

    update: app component and data

---
+++ src/data/features-data.js
@@ -0,0 +1,31 @@
+import CommunityIcon from '../assets/feature-icon-1.png'
+import AnalitycsIcon from '../assets/feature-icon-2.png'
+import QuotesIcon from '../assets/feature-icon-3.png'
+import EventsIcon from '../assets/feature-icon-4.png'
+
+export const features = [
+    {
+        "id": 1,
+        "name": "Exclusive Community",
+        "description": "Join a supportive space where users connect, share experiences, and receive mutual support moderated by mental health professionals.",
+        "img": CommunityIcon
+    },
+    {
+        "id": 2,
+        "name": "Personalized Analysis",
+        "description": "Gain insights into your emotional well-being through tailored assessments and receive personalized recommendations for mental health support.",
+        "img": AnalitycsIcon
+    },
+    {
+        "id": 3,
+        "name": "Specialist Appointment",
+        "description": "Easily book appointments with qualified mental health professionals directly through the app for therapy, counseling, or psychiatric support.",
+        "img": QuotesIcon
+    },
+    {
+        "id": 4,
+        "name": "Exclusive Events",
+        "description": "Access curated workshops, panels, and emotional wellness sessions designed to enhance emotional resilience and overall well-being.",
+        "img": EventsIcon
+    }
+]
---
+++ src/data/testimonials-data.js
@@ -0,0 +1,45 @@
+import ProfileOne from '../assets/testimonial-person-1.png';
+import ProfileTwo from '../assets/testimonial-person-2.png';
+import ProfileThree from '../assets/testimonial-person-3.png';
+import ProfileFour from '../assets/testimonial-person-4.png';
+import ProfileFive from '../assets/testimonial-person-5.jpg';
+import ProfileSix from '../assets/testimonial-person-6.jpg';
+
+export const cards = [
+    {
+        "name": "Emily Rodriguez",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "Director of Operations at Aid for All Company",
+        "profile": ProfileFour
+    },
+    {
+        "name": "Mark Thompson",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "CEO at Compassionate Hands Foundations",
+        "profile": ProfileTwo
+    },
+    {
+        "name": "Steve Jobs",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "Communication Manager At Solidary Foundation",
+        "profile": ProfileThree
+    },
+    {
+        "name": "Alexandra Johnson",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "Communication Manager At Solidary Foundation",
+        "profile": ProfileFive
+    },
+    {
+        "name": "Emily Patel",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "Communication Manager At Solidary Foundation",
+        "profile": ProfileSix
+    },
+    {
+        "name": "Michael Nguyen",
+        "review": "ConnectionLink has transformed the way we operate at 'Aid for All'. Their platform provides invaluable insights into participant and donor data, allowing us to optimize our strategies effectively. Additionally, its user-friendly interface has streamlined our workflow.",
+        "position": "Communication Manager At Solidary Foundation",
+        "profile": ProfileOne
+    }
+]
